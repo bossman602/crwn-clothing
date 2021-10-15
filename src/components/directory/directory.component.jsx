5e0c543c4ffaacea42aaa5fb96e8c234fec65110
bossman602 <bossman602@gmail.com>
2021-10-15 12:34:25 -0400

    updating our app with a shop page, collection preview component and collection item.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,62 +1,68 @@
-import React from 'react';
-import MenuItem from '../menu-item/menu-item.component';
-import './directory.styles.scss'
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+
+import SECTIONS_DATA from "./sections.data";
 
 class Directory extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'shop/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                }
-            ]
-        }
+            sections: SECTIONS_DATA,
+
+            //     {
+            //         title: 'hats',
+            //         // imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            //         imageUrl: 'images/hats.png', // local file in public/images/
+            //         id: 1,
+            //         linkUrl: 'shop/hats'
+
+            //     },
+            //     {
+            //         title: 'jackets',
+            //         // imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            //         imageUrl: 'images/jackets.png', // local file in public/images/
+            //         id: 2,
+            //         linkUrl: 'shop/jackets'
+            //     },
+            //     {
+            //         title: 'sneakers',
+            //         // imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            //         imageUrl: 'images/sneakers.png', // local file in public/images/
+            //         id: 3,
+            //         linkUrl: 'shop/sneakers'
+            //     },
+            //     {
+            //         title: 'womens',
+            //         // imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            //         imageUrl: 'images/womens.png', // local file in public/images/
+            //         size: 'large',
+            //         id: 4,
+            //         linkUrl: 'shop/womens'
+            //     },
+            //     {
+            //         title: 'mens',
+            //         // imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            //         imageUrl: 'images/men.png', // local file in public/images/
+            //         size: 'large',
+            //         id: 5,
+            //         linkUrl: 'shop/mens'
+            //     }
+            //
+
+        };
     }
 
     render() {
         return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({ id, ...otherSectionProps }) => (
-                        <MenuItem key={id} {...otherSectionProps} />
-                    ))
-                }
+            <div className="directory-menu">
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
+                ))}
             </div>
         );
     }
-
 }
-
 
 export default Directory;
