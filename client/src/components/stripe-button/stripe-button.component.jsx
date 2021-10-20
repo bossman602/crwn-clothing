ba9800e2d031464fd9b73eb33a5e13316209e515
bossman602 <bossman602@gmail.com>
2021-10-20 00:41:20 -0400

    added stripe payment backend and additional code for stripe payments to front end.

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51JlbDpBNEtBjYn5JrWAimyUUVfqyp0cYYurGmRrHhyfVvgwuUvPf6KkNo6h6dni0GFtLJeR1NuywtmspWAEOtkSZ00gE9Y8K8M';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful!');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful!');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please make sure you use the provided credit card.'
+            )
+        });
     }
 
     return (
--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -39,12 +39,13 @@ const CheckoutPage = () => {
                 <span>TOTAL: ${total}</span>
 
             </div>
+            <StripeCheckoutButton price={total} />
             <div className='test-warning'>
                 *Please use the following test credit card for payments*
                 <br />
                 4242 4242 4242 4242 - Exp: 01/26 - CVV: 123
             </div>
-            <StripeCheckoutButton price={total} />
+
         </div>
     );
 }
